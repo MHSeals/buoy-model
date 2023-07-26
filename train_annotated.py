99609051b0a37a1599f6a6254180316c7f22e804
Alec Jensen <alecbradyjensen@gmail.com>
2023-07-26 13:57:55 -0500

    add utility for converting solo dataset to yolov8 format. also updated requirements

--- train_annotated.py
+++ train_annotated.py
@@ -1,22 +1,30 @@
 from ultralytics import YOLO
-from roboflow import Roboflow
+import os
+# from roboflow import Roboflow
 
-from secret import roboflow_api_key
+# from secret import roboflow_api_key
 
-rf = Roboflow(api_key=roboflow_api_key)
+# rf = Roboflow(api_key=roboflow_api_key)
 
-project = rf.workspace("mhseals").project("buoys-4naae")
+# project = rf.workspace("mhseals").project("buoys-4naae")
 
-dataset = project.version(6).download('yolov8')
+# dataset = project.version(7).download("yolov8")
+
+class Dataset:
+    def __init__(self, location, version):
+        self.location = os.path.abspath(location)
+        self.version = version
+
+
+dataset = Dataset(location="SYNTHETIC-YOLO", version=1)
 
 model = YOLO("yolov8s.pt")
 
-results = model.train(data=f"{dataset.location}/data.yaml",
-                      imgsz=640,
-                      epochs=600,
-                      batch=8,
-                      name=f"v{dataset.version}",
-                      amp=True
-                      )
-
-model.export(format="torchscript")
+results = model.train(
+    data=f"{dataset.location}/data.yaml",
+    imgsz=640,
+    epochs=600,
+    batch=8,
+    name=f"v{dataset.version}",
+    amp=True,
+)
--- upload.py
+++ upload.py
@@ -6,4 +6,4 @@ rf = Roboflow(api_key=roboflow_api_key)
 
 project = rf.workspace("mhseals").project("buoys-4naae")
 
-project.version(6).deploy(model_type="yolov8", model_path=f"runs/detect/v6/")
+project.version(7).deploy(model_type="yolov8", model_path=f"runs/detect/v7/")
